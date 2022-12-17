c096f457889685973094b0b2e814f677a9c88512
Alvaromorenogil <amorgil0905@g.educaand.es>
2022-12-17 15:52:18 +0100

    1.14

---
+++ ProyectoCoche/src/app/home/home.page.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  cars: any;
+  filtro: string = '';
+  criterioFiltro: string = 'marca';
+
+  
+  onRadioValueChanged(event: any) {
+    console.log("Criterio de búsqueda seleccionado: "+event.detail.value);
+    this.criterioFiltro = event.detail.value;
+  }
+
+  constructor(private httpClient: HttpClient) {
+    this.cars = this.httpClient.get('https://raw.githubusercontent.com/sergiomateos2daw/ProyectoPeliculas/master/car.json');
+  }
+    
+}
---
+++ ProyectoPeliculas/src/app/home/home.page.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  movies: any;
+  filtro: string = '';
+  criterioFiltro: string = 'titulo';
+
+  
+  onRadioValueChanged(event: any) {
+    console.log("Criterio de búsqueda seleccionado: "+event.detail.value);
+    this.criterioFiltro = event.detail.value;
+  }
+
+  constructor(private httpClient: HttpClient) {
+    this.movies = this.httpClient.get('https://raw.githubusercontent.com/sergiomateos2daw/ProyectoPeliculas/master/peliculas.json');
+  }
+    
+}
