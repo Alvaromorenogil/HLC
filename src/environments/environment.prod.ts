4854e7fa6f809cad6f6721e1912472ff83913342
Alvaromorenogil <amorgil0905@g.educaand.es>
2023-03-13 10:34:19 +0100

    finish

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyBwtRfjjO4fvf5kIvxRj8gIk0JD7hFYi84",
+    authDomain: "ejemplo-cloud-firebase-38c58.firebaseapp.com",
+    projectId: "ejemplo-cloud-firebase-38c58",
+    storageBucket: "ejemplo-cloud-firebase-38c58.appspot.com",
+    messagingSenderId: "1056095999804",
+    appId: "1:1056095999804:web:be3ca94d3ef607c17bf039"
+  }
 };
